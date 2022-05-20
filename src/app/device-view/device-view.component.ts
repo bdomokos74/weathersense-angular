048648e16a4d7f1fd0d793cce501aff6d6316101
Balint Domokos <bdomokos74@gmail.com>
2022-05-20 22:44:47 +0200

    New components

---
+++ src/app/device-view/device-view.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+import devicesJson from '../../assets/devices';
+
+@Component({
+  selector: 'app-device-view',
+  templateUrl: './device-view.component.html',
+  styleUrls: ['./device-view.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
+})
+export class DeviceViewComponent implements OnInit {
+  devices: DeviceRow[] = devicesJson.data;
+  columnsToDisplay = ['deviceId', 'modelId', 'status', 'version'];
+  expandedElement: DeviceRow | null = null;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    console.log("devices:", devicesJson);
+  }
+
+}
+
+export interface DeviceRow {
+  deviceId: string,
+  modelId: string,
+  status: string,
+  version: number
+}
