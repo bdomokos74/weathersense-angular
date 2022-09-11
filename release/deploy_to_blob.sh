64f65c1006e169c86467abd491b678799ce85745
Balint Domokos <bdomokos74@gmail.com>
2022-09-11 19:40:16 +0200

    Pipeline - artifact deploy

--- release/deploy_to_blob.sh
+++ release/deploy_to_blob.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-az storage blob upload-batch -s dist/weathersense-angular -d '$web' --account-name $AZURE_GUI_STORAGE_TEST_ACCOUNT
+az storage blob upload-batch -s dist -d '$web' --account-name $AZURE_GUI_STORAGE_TEST_ACCOUNT
